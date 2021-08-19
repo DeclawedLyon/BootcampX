846a85f9968e9c645ce32f6f61a2f24277126648
DeclawedLyon <declan.e.lyons@gmail.com>
2021-08-19 14:33:20 -0600

    sanasizes useer input to sql

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,20 +8,25 @@ const pool = new Pool({
 
 const input = process.argv.splice(2);
 const processInput = (input) => {
+  // store user input
   const cohortName = input[0];
   const resultsLimit = input[1];
-  pool.query(`
-    SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-    FROM students
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name LIKE '%${process.argv[2]}%'
-    LIMIT ${process.argv[3] || 5};
-  `)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  const inputArray = [cohortName, resultsLimit];
+
+  const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`
+  pool.query(queryString, inputArray)
+  .then(res => {
+    res.rows.forEach(user => {
+     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
 }
 processInput(input);
 
