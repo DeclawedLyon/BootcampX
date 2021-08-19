da84b0c7fc49b16104c522759e0218f777ff2d6a
DeclawedLyon <declan.e.lyons@gmail.com>
2021-08-19 10:07:45 -0600

    connects app to database in BootcampX

--- 4_queries/4_average_assistance_request_wait_time.sql
+++ 4_queries/4_average_assistance_request_wait_time.sql
@@ -1,6 +1,2 @@
-SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
-FROM assistance_requests
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-GROUP BY cohorts.name
-ORDER BY total_duration;
+SELECT avg(started_at-created_at) as average_wait_time
+FROM assistance_requests;
